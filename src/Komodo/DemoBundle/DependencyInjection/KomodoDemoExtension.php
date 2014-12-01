12197a530d5cdee9b374a123e580325d29f28250
Heriberto Monterrubio <heriberto.monterrubio@komodot.com.mx>
2014-12-01 13:00:16 -0600

    Instalacion de Symfony 2.6

--- src/Komodo/DemoBundle/DependencyInjection/KomodoDemoExtension.php
+++ src/Komodo/DemoBundle/DependencyInjection/KomodoDemoExtension.php
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\Loader;
 class KomodoDemoExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
